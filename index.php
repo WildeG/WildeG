07733b74b36bc2326de366fc2c1d6aadbbf635a5
Lilly420 <eredoran@gmail.com>
2015-06-16 20:07:09 +0300

    ксс, логин, регистрация

---
+++ index.php
@@ -0,0 +1,28 @@
+﻿<head>
+		<meta http-equiv="Content-Type" content="text/html"; charset='utf-8'>
+		<link rel="shortcut icon" href="http://www.sdws.ru/favicon.ico" type="image/x-icon">
+		<title>Вход</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<form id="login" action="login.php" method="post" enctype="multipart/form-data"></form>
+		<div id="logpage">
+			<div id="login">
+				<center><h1>Вход</h1></center>
+				<a class="inscriptions">Логин:</a><br>
+				<input class="field" size="30" form="login" type="text"> <br>
+				<a class="inscriptions">Пароль:</a><br>
+				<input class="field" size="30" form="login" type="password"> <br>
+				<input id="button" value="Вход" form="login" type="submit"> <br>
+				<a href="registration.html">Нет учетной записи? </a>
+				<a href="forgot.html">Забыли пароль? </a> <br>
+			</div>
+			<div id="copyright">
+				<strong>Copyright © 2015. Михайлов Олег. Все права защищены.</strong>
+				<p>Перепечатка материалов и использование их в любой форме, в том 
+				числе и в электронных СМИ, возможны только с письменного разрешения 
+				администрации сайта. При этом ссылка на сайт обязательна.</p>
+			</div> 
+		</div>
+	
+</body>
